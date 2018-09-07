350fbfd561075a91b2a9536d0ef40848a2a183de
Valeriy_V_Bagaev <v.bagaev@sarrc.ru>
2018-09-07 09:35:08 +0400

    ff

---
+++ src/app/car/car.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car',
+  templateUrl: './car.component.html',
+  styleUrls: ['./car.component.css']
+})
+export class CarComponent {
+
+  carName = 'Ford';
+  carYear = 2017;
+
+  getName() {
+    return this.carName;
+  }
+
+}
