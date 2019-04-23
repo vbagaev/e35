2d503b8c2a040e274007df99a07c7219468149e2
Валерий Багаев <bagaevvald@outlook.com>
2019-04-23 09:55:14 +0400

    Create q.js

---
+++ q.js
@@ -0,0 +1,5 @@
+searchOrg(
+    [
+        { "id": "1", "order_id": "5", "title": "\u0413\u043e\u0440\u043e\u0434\u0441\u043a\u043e\u0439 \u0440\u0430\u0441\u0447\u0435\u0442\u043d\u044b\u0439 \u0446\u0435\u043d\u0442\u0440 (\u0421\u0430\u0440\u0420\u0426)", "link": "sarrc", "logo": "logo-sarrc", "active": "1", "ais": "1", "organizationid": null, "code": "00000080" },
+        { "id": "2", "order_id": "10", "title": "\u0421\u0430\u0440\u0420\u0426 \u0420\u0435\u0441\u0443\u0440\u0441", "link": "sarrcresurs", "logo": "rco", "active": "1", "ais": "1", "organizationid": null, "code": "\u04119001675" }
+    ])
